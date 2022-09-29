c5d9b8106e609044772d15a464bfab809e8a66ab
Megan Honey <mjthoney@gmail.com>
2022-09-29 01:55:30 +0000

    Complete command line survey function

---
+++ survey.js
@@ -0,0 +1,48 @@
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+let answers = {};
+
+rl.question("What's your name? Nicknames are also acceptable :) ", (answer) => {
+  answers.name = answer;
+  console.log(`Thank you, ${answer}.`);
+  
+  rl.question("What's an activity you like doing? ", (answer) => {
+    answers.hobby = answer;
+
+    rl.question("What do you listen do while doing that? ", (answer) => {
+      answers.music = answer;
+
+      rl.question("Which meal is your favourite (eg: dinner, brunch, etc.) ", (answer) => {
+        answers.meal = answer;
+
+        rl.question("What's your favourite thing to eat for that meal? ", (answer) => {
+          answers.food = answer;
+
+          rl.question("Which sport is your absolute favourite? ", (answer) => {
+            answers.sport = answer;
+
+            rl.question("What is your superpower? In a few words, tell us what you are amazing at! ", (answer) => {
+              answers.power = answer;
+              console.log(`${answers.name} loves listening to ${answers.music} while ${answers.hobby.toLowerCase()}, devouring ${answers.foodtoLowerCase()} for ${answers.mealtoLowerCase()}, prefers ${answers.sporttoLowerCase()} over any other sport, and is amazing at ${answers.powertoLowerCase()}.`);
+
+              rl.close()
+        
+            });
+      
+          });
+    
+        });
+  
+      });
+
+    });
+
+  });
+
+});
+
